f159faaad9fefab356017edd6729a46591ee5c8d
h <besqer996@gnu.ac.kr>
2020-05-28 04:58:46 +0900

    Make embed.rs quiet

--- src/embed.rs
+++ src/embed.rs
@@ -1,6 +1,7 @@
+#![cfg(feature = "embed-any")]
+
 use std::path::PathBuf;
 
-#[cfg(feature = "embed-any")]
 pub fn path_for_file(filename: &str) -> PathBuf {
     PathBuf::from(env!("CARGO_MANIFEST_DIR"))
         .join("files")
