ecb755a483feda5c90435bd7997ed8f22333b409
h <besqer996@gnu.ac.kr>
2020-06-06 01:35:03 +0900

    Add more functions in FaceComparer

--- src/face_encoding/compare.rs
+++ src/face_encoding/compare.rs
@@ -1,29 +1,37 @@
+use std::collections::HashMap;
+
 use super::encoding::FaceEncoding;
 
 #[derive(Default)]
 pub struct FaceComparer {
-    keys: Vec<String>,
-    values: Vec<FaceEncoding>,
+    seed: usize,
+    names: HashMap<usize, String>,
+    values: HashMap<usize, FaceEncoding>,
 }
 
 impl FaceComparer {
     pub fn insert(&mut self, name: String, value: FaceEncoding) {
-        self.keys.push(name);
-        self.values.push(value);
+        let name_str = name.as_str();
+        if let Some((&key, _)) = self.names.iter().find(|(_, n)| n.as_str() == name_str) {
+            self.values.insert(key, value);
+        } else {
+            self.names.insert(self.seed, name);
+            self.values.insert(self.seed, value);
+            self.seed += 1;
+        }
     }
 
     pub fn find(&self, face: &FaceEncoding) -> Option<usize> {
         const TOLERANCE: f64 = 0.6;
 
-        if let Some((index, x)) = self
+        if let Some((key, x)) = self
             .values
             .iter()
-            .enumerate()
             .map(|(i, f)| (i, f.distance(face)))
             .min_by(|(_, x), (_, y)| x.partial_cmp(y).unwrap())
         {
             if x <= TOLERANCE {
-                Some(index)
+                Some(*key)
             } else {
                 None
             }
@@ -32,7 +40,22 @@ impl FaceComparer {
         }
     }
 
-    pub fn get_name_unchecked(&self, key: usize) -> &str {
-        &self.keys[key]
+    pub fn get_name_unchecked(&mut self, key: &usize) -> &str {
+        &self.names[key]
+    }
+
+    pub fn remove_key(&mut self, key: &usize) {
+        self.names.remove(key);
+        self.values.remove(key);
+    }
+
+    pub fn remove_name(&mut self, name: &str) {
+        if let Some((&key, _)) = self.names.iter().find(|(_, n)| n.as_str() == name) {
+            self.remove_key(&key);
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.names.len()
     }
 }
