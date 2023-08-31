d09d7610b60c10c3017ee536019fdb20862226ad
Ho Kim <ho.kim@ulagbulag.io>
2023-08-31 14:31:03 +0900

    Make non-debian linux distros use cblas

--- build.rs
+++ build.rs
@@ -1,14 +1,23 @@
+fn is_debian() -> bool {
+    cfg!(target_os = "linux")
+        && ::sys_info::linux_os_release()
+            .map(|os| os.id_like.map(|id| id == "debian").unwrap_or_default())
+            .unwrap_or_default()
+}
+
 fn main() {
     let mut config = cpp_build::Config::new();
     println!("cargo:rerun-if-changed=./files");
 
-    #[cfg(target_family="windows")] {
+    #[cfg(target_family = "windows")]
+    {
         let root_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
         let windows = std::path::PathBuf::from(root_dir)
             .join("external-libs")
             .join("windows");
 
-        #[cfg(target_env="gnu")] {
+        #[cfg(target_env = "gnu")]
+        {
             config.flag("-Os");
             config.flag("-Wa,-mbig-obj");
         }
@@ -18,20 +27,24 @@ fn main() {
         println!("cargo:rustc-link-lib=blas");
         println!("cargo:rustc-link-lib=lapack");
         println!("cargo:rustc-link-lib=static=dlib");
-    } 
+    }
 
-    #[cfg(not(target_family="windows"))] {
+    #[cfg(not(target_family = "windows"))]
+    {
+        if is_debian() {
+            println!("cargo:rustc-link-lib=blas");
+        } else {
+            println!("cargo:rustc-link-lib=cblas");
+        }
+
         println!("cargo:rustc-link-lib=dlib");
-        println!("cargo:rustc-link-lib=blas");
         println!("cargo:rustc-link-lib=lapack");
-    } 
+    }
 
     if let Ok(paths) = std::env::var("DEP_DLIB_INCLUDE") {
         for path in std::env::split_paths(&paths) {
             config.include(path);
         }
     }
-    config
-    .flag("-std=c++14")
-    .build("src/lib.rs");
+    config.flag("-std=c++14").build("src/lib.rs");
 }
